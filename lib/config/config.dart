03a607feb1ba3559f9e17cdfa572ac5a6f5a95df
Niranjan Pradeep <niranjanpradeep.dev@gmail.com>
2026-03-09 19:27:13 +0530

    Updated config settings

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   // Check your IP if you are on a real device.
   // Use 10.0.2.2 for Android Emulator.
-  static const String baseUrl = "http://192.168.1.36:8000";
+  static const String baseUrl = "http://192.168.0.105:8000";
 }
